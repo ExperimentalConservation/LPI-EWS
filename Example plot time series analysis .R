0496906bc6181dd20838c256a2600ee755b998c9
chrisclementsresearch@gmail.com <chrisadmin@ieu0947.local>
2015-11-25 10:53:34 +0100

    Example of how EWS are calculated
    Example plot of simulated data showing how news are calculated using
    the three different methods

--- Example plot time series analysis .R
+++ Example plot time series analysis .R
@@ -53,75 +53,79 @@ time.series<-cbind(time.series,
 
 
 pdf("~/Desktop/LivingPlanet index early warnings/plots/Example ews plots.pdf",width=4, height=9)
-par(mfrow=c(4,1), mar=c(1,4,0.5,1))
-##plot time series and gam smoothing
-plot(time.series$x, time.series$y, type="n", xlab="", ylab="Abundance", xaxt="n", xlim=c(0, 22), ylim=c(0, 150))
-polygon(x=c(10,15, 15, 10),y=c(-10, -10, 170, 170), col= adjustcolor("darkgoldenrod2 ",alpha=0.4), lty=0)
-lines(time.series$x, time.series$y, type="b")
-colour <- ifelse(time.series$gam<0,"red","blue")
-segments(time.series$x[x-1],
-		time.series$fit[x-1],
-		time.series$x[x+1],
-		time.series$fit[x+1],
-		col=colour, lwd=2)
-legend("topleft", 
-		legend=c("Time series", "GAM fit - not declining", "GAM fit - declining"), 
-		lty=1, 
-		pch=c(1, NA, NA),
-		col=c("black", "blue", "red"), bty="n")
-legend("topright", legend="a.", bty="n", cex=1.5)
-
-##plot the generic ews with gam smoothing
-gam.ar1<-gam_smoothing(time.series$x[!is.na(time.series$ar1)], time.series$ar1[!is.na(time.series$ar1)], 5)
-colour.ar1 <- ifelse(gam.ar1$sign>0,"red","blue")
-plot(time.series$x, time.series$ar1, type="n", xlab="Time", ylab="ar1", xaxt="n", xlim=c(0, 22), ylim=c(-0.8, 1.3))
-polygon(x=c(10,15, 15, 10),y=c(-10, -10, 170, 170), col= adjustcolor("darkgoldenrod2 ",alpha=0.4), lty=0)
-lines(time.series$x, time.series$ar1, type="l")
-segments(gam.ar1$years[x-1],
-		gam.ar1$fit[x-1],
-		gam.ar1$years[x+1],
-		gam.ar1$fit[x+1],
-		col=colour.ar1, lwd=2)
-legend("topleft", legend=c("ar1", "GAM fit - not increasing", "GAM fit - increasing"), lty=1, col=c("black", "blue", "red"), bty="n")
-legend("topright", legend="b.", bty="n", cex=1.5)
-
-##plot the composite ews, threshold crossed version
-plot(comp.ews$time, comp.ews$metric.score, type="n", col="skyblue3", ylim=c(0, 7), lwd=2, xaxt="n", ylab="Composite EWS", xlim=c(0, 22))
-polygon(x=c(10,15, 15, 10),y=c(-10, -10, 170, 170), col= adjustcolor("darkgoldenrod2 ",alpha=0.4), lty=0)
-lines(comp.ews$time, comp.ews$metric.score, type="l", col="skyblue3", lwd=2)
-lines(comp.ews$time, comp.ews$rolling.mean, type="l", lwd=1.5)
-lines(comp.ews$time, comp.ews$rolling.mean+(2*comp.ews$rolling.sd), type="l", lty="dashed", lwd=1.5)
-lines(comp.ews$time[which(comp.ews$threshold.crossed==1)], comp.ews$metric.score[which(comp.ews$threshold.crossed==1)], type="p", pch=19, col="maroon")
-legend("topleft", legend=c("Rolling mean", 
-							"Composite metric score",
-							"Threshold value",
-							"Thewshold crossed"), 
-							lty=c("solid","solid","dashed",NA),
-							pch=c(NA, NA, NA, 19),
-							col=c("black", "skyblue3", "black", "maroon"), bty="n")
-legend("topright", legend="c.", bty="n", cex=1.5)
-
-##plot the composite ews, gam version 
-par(mar=c(4,4,0.5,1))
-gam.comp<-gam_smoothing(comp.ews$time[!is.na(comp.ews$metric.score)], 
-						comp.ews$metric.score[!is.na(comp.ews$metric.score)],
-						6)
-plot(comp.ews$time, comp.ews$metric.score, type="n", col="skyblue3", ylim=c(0, 7), lwd=2, xlab="Time", ylab="Composite EWS", xlim=c(0, 22))
-polygon(x=c(10,15, 15, 10),y=c(-10, -10, 170, 170), col= adjustcolor("darkgoldenrod2 ",alpha=0.4), lty=0)
-lines(comp.ews$time, comp.ews$metric.score, type="l", col="skyblue3", lwd=2)
-colour.comp<-ifelse(gam.comp$sign>0,"red","blue")
-segments(gam.comp $years[x-1],
-		gam.comp $fit[x-1],
-		gam.comp $years[x+1],
-		gam.comp $fit[x+1],
-		col=colour.comp, lwd=2)
-legend("topleft", 
-	legend=c("Composite metric score","GAM fit - not increasing", "GAM fit - increasing"), 
-	lty=1,
-	lwd=1.5,
-	col=c("skyblue3", "blue", "red"), bty="n")
-legend("topright", legend="d.", bty="n", cex=1.5)
-
+	par(mfrow=c(4,1), mar=c(1,4,0.5,1))
+	##plot time series and gam smoothing
+	plot(time.series$x, time.series$y, type="n", xlab="", ylab="Abundance", xaxt="n", xlim=c(0, 23.5), ylim=c(0, 150))
+	polygon(x=c(10,15, 15, 10),y=c(-10, -10, 170, 170), col= adjustcolor("darkgoldenrod2 ",alpha=0.4), lty=0)
+	lines(time.series$x, time.series$y, type="b")
+	polygon(x=c(17,22, 22, 17),y=c(-10, -10, 170, 170), col= adjustcolor("grey ",alpha=0.6), lty=0)
+	colour <- ifelse(time.series$gam<0,"red","blue")
+	segments(time.series$x[x-1],
+			time.series$fit[x-1],
+			time.series$x[x+1],
+			time.series$fit[x+1],
+			col=colour, lwd=2)
+	legend("topleft", 
+			legend=c("Time series", "GAM fit - not declining", "GAM fit - declining"), 
+			lty=1, 
+			pch=c(1, NA, NA),
+			col=c("black", "blue", "red"), bty="n")
+	legend("topright", legend="a.", bty="n", cex=1.5)
+	
+	##plot the generic ews with gam smoothing
+	gam.ar1<-gam_smoothing(time.series$x[!is.na(time.series$ar1)], time.series$ar1[!is.na(time.series$ar1)], 5)
+	colour.ar1 <- ifelse(gam.ar1$sign>0,"red","blue")
+	plot(time.series$x, time.series$ar1, type="n", xlab="Time", ylab="ar1", xaxt="n", xlim=c(0, 23.5), ylim=c(-0.9, 1.3))
+	polygon(x=c(10,15, 15, 10),y=c(-10, -10, 170, 170), col= adjustcolor("darkgoldenrod2 ",alpha=0.4), lty=0)
+	lines(time.series$x, time.series$ar1, type="b")
+	segments(gam.ar1$years[x-1],
+			gam.ar1$fit[x-1],
+			gam.ar1$years[x+1],
+			gam.ar1$fit[x+1],
+			col=colour.ar1, lwd=2)
+	polygon(x=c(17,22, 22, 17),y=c(-10, -10, 170, 170), col= adjustcolor("grey ",alpha=0.6), lty=0)
+	legend("topleft", legend=c("ar1", "GAM fit - not increasing", "GAM fit - increasing"), lty=1, col=c("black", "blue", "red"), bty="n")
+	legend("topright", legend="b.", bty="n", cex=1.5)
+	
+	##plot the composite ews, threshold crossed version
+	plot(comp.ews$time, comp.ews$metric.score, type="n", col="skyblue3", ylim=c(0, 7), lwd=2, xaxt="n", ylab="Composite EWS", xlim=c(0, 23.5))
+	polygon(x=c(10,15, 15, 10),y=c(-10, -10, 170, 170), col= adjustcolor("darkgoldenrod2 ",alpha=0.4), lty=0)
+	lines(comp.ews$time, comp.ews$metric.score, type="l", col="skyblue3", lwd=2)
+	lines(comp.ews$time, comp.ews$rolling.mean, type="l", lwd=1.5)
+	lines(comp.ews$time, comp.ews$rolling.mean+(2*comp.ews$rolling.sd), type="l", lty="dashed", lwd=1.5)
+	lines(comp.ews$time[which(comp.ews$threshold.crossed==1)], comp.ews$metric.score[which(comp.ews$threshold.crossed==1)], type="p", pch=19, col="maroon")
+	polygon(x=c(17,22, 22, 17),y=c(-10, -10, 170, 170), col= adjustcolor("grey ",alpha=0.6), lty=0)
+	legend("topleft", legend=c("Running mean", 
+								"Composite metric score",
+								"Threshold value",
+								"Threshold crossed"), 
+								lty=c("solid","solid","dashed",NA),
+								pch=c(NA, NA, NA, 19),
+								col=c("black", "skyblue3", "black", "maroon"), bty="n")
+	legend("topright", legend="c.", bty="n", cex=1.5)
+	
+	##plot the composite ews, gam version 
+	par(mar=c(4,4,0.5,1))
+	gam.comp<-gam_smoothing(comp.ews$time[!is.na(comp.ews$metric.score)], 
+							comp.ews$metric.score[!is.na(comp.ews$metric.score)],
+							6)
+	plot(comp.ews$time, comp.ews$metric.score, type="n", col="skyblue3", ylim=c(0, 7), lwd=2, xlab="Time", ylab="Composite EWS", xlim=c(0, 23.5))
+	polygon(x=c(10,15, 15, 10),y=c(-10, -10, 170, 170), col= adjustcolor("darkgoldenrod2 ",alpha=0.4), lty=0)
+	lines(comp.ews$time, comp.ews$metric.score, type="l", col="skyblue3", lwd=2)
+	colour.comp<-ifelse(gam.comp$sign>0,"red","blue")
+	segments(gam.comp $years[x-1],
+			gam.comp $fit[x-1],
+			gam.comp $years[x+1],
+			gam.comp $fit[x+1],
+			col=colour.comp, lwd=2)
+	polygon(x=c(17,22, 22, 17),y=c(-10, -10, 170, 170), col= adjustcolor("grey ",alpha=0.6), lty=0)
+	legend("topleft", 
+		legend=c("Composite metric score","GAM fit - not increasing", "GAM fit - increasing"), 
+		lty=1,
+		lwd=1.5,
+		col=c("skyblue3", "blue", "red"), bty="n")
+	legend("topright", legend="d.", bty="n", cex=1.5)
+	
 dev.off()
 
 
